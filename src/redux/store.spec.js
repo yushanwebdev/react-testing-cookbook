3471e396bc87904cdfd1101c69ea5626f98dae49
yushan <yushanwebdev@gmail.com>
2021-12-28 20:42:21 +0530

    feat: The Redux Store - Multiple Actions advanced

--- src/redux/store.spec.js
+++ src/redux/store.spec.js
@@ -3,27 +3,70 @@ import expect from "expect";
 
 describe("store", () => {
   it("should work with a series of actions", () => {
-    store.dispatch({
-      type: "ADD_QUOTE_BY_ID",
-      payload: {
-        text: "The best way to cheer yourself up is to try to cheer somebody else up.",
-        author: "Mark Twain",
-        id: 1,
-        likeCount: 24,
-      },
-    });
-
-    const actual = store.getState();
-    const expected = {
-      quotes: [
-        {
+    const actions = [
+      {
+        type: "ADD_QUOTE_BY_ID",
+        payload: {
           text: "The best way to cheer yourself up is to try to cheer somebody else up.",
           author: "Mark Twain",
           id: 1,
           likeCount: 24,
         },
+      },
+      {
+        type: "ADD_QUOTE_BY_ID",
+        payload: {
+          text: "Whatever you are, be a good one.",
+          author: "Abraham Lincoln",
+          id: 2,
+          likeCount: 0,
+        },
+      },
+      {
+        type: "REMOVE_QUOTE_BY_ID",
+        payload: {
+          id: 1,
+        },
+      },
+      {
+        type: "LIKE_QUOTE_BY_ID",
+        payload: {
+          id: 2,
+        },
+      },
+      {
+        type: "LIKE_QUOTE_BY_ID",
+        payload: {
+          id: 2,
+        },
+      },
+      {
+        type: "UNLIKE_QUOTE_BY_ID",
+        payload: {
+          id: 2,
+        },
+      },
+      {
+        type: "UPDATE_THEME_COLOR",
+        payload: {
+          color: "#777777",
+        },
+      },
+    ];
+
+    actions.forEach((action) => store.dispatch(action));
+
+    const actual = store.getState();
+    const expected = {
+      quotes: [
+        {
+          text: "Whatever you are, be a good one.",
+          author: "Abraham Lincoln",
+          id: 2,
+          likeCount: 1,
+        },
       ],
-      theme: { color: "#5DC4C6" },
+      theme: { color: "#777777" },
     };
     expect(actual).toEqual(expected);
   });
