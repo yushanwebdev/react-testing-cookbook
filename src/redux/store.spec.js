48a634f0ceaf601072b90d7b6b36d5b5359be459
yushan <yushanwebdev@gmail.com>
2021-12-28 20:49:43 +0530

    feat: The Redux Store - Initial State

--- src/redux/store.spec.js
+++ src/redux/store.spec.js
@@ -2,6 +2,18 @@ import { store } from "./store";
 import expect from "expect";
 
 describe("store", () => {
+  it("should initialize", () => {
+    const actual = store.getState();
+    const expected = {
+      quotes: [],
+      theme: {
+        color: "#5DC4C6",
+      },
+    };
+
+    expect(actual).toEqual(expected);
+  });
+
   it("should work with a series of actions", () => {
     const actions = [
       {
